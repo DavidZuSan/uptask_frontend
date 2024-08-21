7d71d33847dc8f6e293d7d923d3b10f74c73fbdf
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-08-21 18:36:49 +0200

    redireccion a pagina principal despues de enviar el formulario de actividaddes

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import ProjectForm from "@/components/projects/ProjectForm";
 import { ProjectFormData } from "@/types/index";
 import { createProject } from "@/api/ProjectAPI";
 
 export default function CreateProjectView() {
+  const navigate = useNavigate();
   const initialValues: ProjectFormData = {
     projectName: "",
     clientName: "",
@@ -18,8 +19,9 @@ export default function CreateProjectView() {
     defaultValues: initialValues,
   });
 
-  const handleForm = (data: ProjectFormData) => {
-    createProject(data);
+  const handleForm = async (data: ProjectFormData) => {
+    await createProject(data);
+    navigate("/");
   };
   return (
     <>
