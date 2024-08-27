cea140a35078e50f3323a77a32e7a898d848948e
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-08-27 18:33:35 +0200

    Visualizacion de Tareas y Adminstrador de Tareas I

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,19 +1,33 @@
 import { z } from "zod";
 
+/** Task */
+export const taskStatusSchema = z.enum(["peding", "onHold", "inProgress", "underReview","completed"])
+
+export const taskSchema = z.object({
+    _id: z.string(),
+    name: z.string(),
+    description: z.string(),
+    project: z.string(),
+    status: taskStatusSchema,
+})
+
+export type Task = z.infer<typeof taskSchema>;
+export type TaskFormData = Pick<Task, 'name' | 'description'>;
+
 /** Projects */
-export const ProjectSchema = z.object({
+export const projectSchema = z.object({
     _id: z.string(),
     projectName: z.string(),
     clientName: z.string(),
     description: z.string(),
 })
 
-export const dashboardProjectsSchema = z.array(ProjectSchema.pick({
+export const dashboardProjectsSchema = z.array(projectSchema.pick({
     _id: true,
     projectName: true,
     clientName: true,
     description: true,
 }));
 
-export type Project = z.infer<typeof ProjectSchema>;
+export type Project = z.infer<typeof projectSchema>;
 export type ProjectFormData = Pick<Project, 'projectName' |'clientName' | 'description' >
--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -6,13 +6,11 @@ import EditProjectFrom from "@/components/projects/EditProjectFrom";
 export default function EditProjectView() {
   const params = useParams();
   const projectId = params.projectId!;
-
   const { data, isLoading, isError } = useQuery({
     queryKey: ["editProject", projectId],
     queryFn: () => getProjectById(projectId),
     retry: false,
   });
-
   if (isLoading) return "Cargando proyecto...";
   if (isError) return <Navigate to="/404" />;
   if (data) return <EditProjectFrom data={data} projectId={projectId} />;
