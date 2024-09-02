f3b1cf4f798d09e6aa0ccd10be6aac4b87b201b4
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-09-02 11:22:48 +0200

    Funciones de Visionado terminadas

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -13,7 +13,10 @@ export async function createTask({formData, projectId} : Pick<TaskAPI, 'formData
     try {
         const url = `/projects/${projectId}/tasks`
         const { data } = await api.post<string>(url, formData)
-        return data
+        const response = taskSchema.safeParse(data)
+        if(response.success) {
+            return response.data
+        }
     } catch (error) {
         if(isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
@@ -49,6 +52,18 @@ export async function updateTask({projectId, taskId, formData}: Pick<TaskAPI, 'p
 }
 
 export async function deleteTask({projectId, taskId} : Pick<TaskAPI, 'projectId' | 'taskId'>) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}`
+        const { data } = await api.delete<string>(url)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function updateStatus({projectId, taskId} : Pick<TaskAPI, 'projectId' | 'taskId'>) {
     try {
         const url = `/projects/${projectId}/tasks/${taskId}`
         const { data } = await api.delete<string>(url)
--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import {
   Dialog,
   DialogPanel,
@@ -6,9 +6,12 @@ import {
   Transition,
   TransitionChild,
 } from "@headlessui/react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useLocation, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getTaskById } from "@/api/TaskAPI";
+import { toast } from "react-toastify";
+import { formatDate } from "@/utils/utils";
+import { statusTranslations } from "@/locales/es";
 
 export default function TaskModalDetails() {
   const params = useParams();
@@ -16,64 +19,94 @@ export default function TaskModalDetails() {
   const navigate = useNavigate();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const taskId = queryParams.get("viewTask");
+  const taskId = queryParams.get("viewTask")!;
+
   const show = taskId ? true : false;
-  const { data } = useQuery({
+
+  const { data, isError, error } = useQuery({
     queryKey: ["tasks", taskId],
     queryFn: () => getTaskById({ projectId, taskId }),
+    enabled: !!taskId,
+    retry: false,
   });
-  return (
-    <>
-      <Transition appear show={show} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10"
-          onClose={() => navigate(location.pathname, { replace: true })}
-        >
-          <TransitionChild
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black/60" />
-          </TransitionChild>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <TransitionChild
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <DialogPanel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
-                  <p className="text-sm text-slate-400">Agregada el: </p>
-                  <p className="text-sm text-slate-400">
-                    Última actualización:{" "}
-                  </p>
-                  <DialogTitle
-                    as="h3"
-                    className="font-black text-4xl text-slate-600 my-5"
-                  >
-                    Titulo aquí
-                  </DialogTitle>
-                  <p className="text-lg text-slate-500 mb-2">Descripción:</p>
-                  <div className="my-5 space-y-3">
-                    <label className="font-bold">Estado Actual:</label>
-                  </div>
-                </DialogPanel>
-              </TransitionChild>
+  // Manejar la navegación en caso de error usando useEffect
+  useEffect(() => {
+    if (isError) {
+      toast.error(error.message, { toastId: "error" });
+      navigate(`/projects/${projectId}`);
+    }
+  }, [isError, error, navigate, projectId]);
+
+  if (data)
+    return (
+      <>
+        <Transition appear show={show} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-10"
+            onClose={() => navigate(location.pathname, { replace: true })}
+          >
+            <TransitionChild
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black/60" />
+            </TransitionChild>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <TransitionChild
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <DialogPanel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
+                    <p className="text-sm text-slate-400">
+                      Agregada el: {formatDate(data.createdAt)}
+                    </p>
+                    <p className="text-sm text-slate-400">
+                      Última actualización: {formatDate(data.updatedAt)}
+                    </p>
+                    <DialogTitle
+                      as="h3"
+                      className="font-black text-4xl text-slate-600 my-5"
+                    >
+                      {data.name}
+                    </DialogTitle>
+                    <p className="text-lg text-slate-500 mb-2">
+                      Descripción: {data.description}
+                    </p>
+                    <div className="my-5 space-y-3">
+                      <label className="font-bold">Estado Actual:</label>
+                      <select
+                        className="w-full p-3 bg-white border border-gay-300"
+                        defaultValue={data.status}
+                      >
+                        {Object.entries(statusTranslations).map(
+                          ([key, value]) => (
+                            <option key={key} value={key}>
+                              {value}
+                            </option>
+                          )
+                        )}
+                      </select>
+                    </div>
+                  </DialogPanel>
+                </TransitionChild>
+              </div>
             </div>
-          </div>
-        </Dialog>
-      </Transition>
-    </>
-  );
+          </Dialog>
+        </Transition>
+      </>
+    );
 }
