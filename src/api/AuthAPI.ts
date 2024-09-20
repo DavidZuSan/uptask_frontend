38fb8d1ffe0850041da50ccc2f8003663cf57c40
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-09-20 11:04:05 +0200

    Actualizaciones sobre Consulta de Cooperadores

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,15 +1,15 @@
-import api from '@/lib/axios';
-import { isAxiosError } from 'axios';
-import { ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationCodeForm, User, UserLoginForm, UserRegistrationForm, userSchema } from '../types';
+import api from "@/lib/axios";
+import { isAxiosError } from "axios";
+import { CheckPasswordForm, ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm, userSchema } from "../types";
 
 export async function createAccount(formData: UserRegistrationForm) {
     try {
         const url = '/auth/create-account'
-        const { data } = await api.post<string>(url, formData);
-        return data;
+        const { data } = await api.post<string>(url, formData)
+        return data
     } catch (error) {
         if(isAxiosError(error) && error.response) {
-            throw new Error(error.response.data.error);
+            throw new Error(error.response.data.error)
         }
     }
 }
@@ -17,11 +17,11 @@ export async function createAccount(formData: UserRegistrationForm) {
 export async function confirmAccount(formData: ConfirmToken) {
     try {
         const url = '/auth/confirm-account'
-        const { data } = await api.post<string>(url, formData);
-        return data;
+        const { data } = await api.post<string>(url, formData)
+        return data
     } catch (error) {
         if(isAxiosError(error) && error.response) {
-            throw new Error(error.response.data.error);
+            throw new Error(error.response.data.error)
         }
     }
 }
@@ -29,11 +29,11 @@ export async function confirmAccount(formData: ConfirmToken) {
 export async function requestConfirmationCode(formData: RequestConfirmationCodeForm) {
     try {
         const url = '/auth/request-code'
-        const { data } = await api.post<string>(url, formData);
-        return data;
+        const { data } = await api.post<string>(url, formData)
+        return data
     } catch (error) {
         if(isAxiosError(error) && error.response) {
-            throw new Error(error.response.data.error);
+            throw new Error(error.response.data.error)
         }
     }
 }
@@ -41,11 +41,12 @@ export async function requestConfirmationCode(formData: RequestConfirmationCodeF
 export async function authenticateUser(formData: UserLoginForm) {
     try {
         const url = '/auth/login'
-        const { data } = await api.post<string>(url, formData);
-        return data;
+        const { data } = await api.post<string>(url, formData)
+        localStorage.setItem('AUTH_TOKEN', data)
+        return data
     } catch (error) {
         if(isAxiosError(error) && error.response) {
-            throw new Error(error.response.data.error);
+            throw new Error(error.response.data.error)
         }
     }
 }
@@ -53,11 +54,11 @@ export async function authenticateUser(formData: UserLoginForm) {
 export async function forgotPassword(formData: ForgotPasswordForm) {
     try {
         const url = '/auth/forgot-password'
-        const { data } = await api.post<string>(url, formData);
-        return data;
+        const { data } = await api.post<string>(url, formData)
+        return data
     } catch (error) {
         if(isAxiosError(error) && error.response) {
-            throw new Error(error.response.data.error);
+            throw new Error(error.response.data.error)
         }
     }
 }
@@ -65,38 +66,50 @@ export async function forgotPassword(formData: ForgotPasswordForm) {
 export async function validateToken(formData: ConfirmToken) {
     try {
         const url = '/auth/validate-token'
-        const { data } = await api.post<string>(url, formData);
-        return data;
+        const { data } = await api.post<string>(url, formData)
+        return data
     } catch (error) {
         if(isAxiosError(error) && error.response) {
-            throw new Error(error.response.data.error);
+            throw new Error(error.response.data.error)
         }
     }
 }
 
-export async function updatePasswordWithToken({formData, token}: {formData: NewPasswordForm, token: ConfirmToken['token'] }) {
+export async function updatePasswordWithToken({formData, token}: {formData: NewPasswordForm, token: ConfirmToken['token']}) {
     try {
+        
         const url = `/auth/update-password/${token}`
-        const { data } = await api.post<string>(url, formData);
-        return data;
+        const { data } = await api.post<string>(url, formData)
+        return data
     } catch (error) {
         if(isAxiosError(error) && error.response) {
-            throw new Error(error.response.data.error);
+            throw new Error(error.response.data.error)
         }
     }
 }
 
-export async function getUser() { 
+export async function getUser() {
     try {
-        const url = '/auth/user'
-        const { data } = await api(url);
-        const response = userSchema.safeParse(data);
+        const { data } = await api('/auth/user')
+        const response = userSchema.safeParse(data)
         if(response.success) {
-            return response.data;
+            return response.data
         }
     } catch (error) {
         if(isAxiosError(error) && error.response) {
-            throw new Error(error.response.data.error);
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function checkPassword(formData: CheckPasswordForm) {
+    try {
+        const url = '/auth/check-password'
+        const { data } = await api.post<string>(url, formData)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
         }
     }
 }
