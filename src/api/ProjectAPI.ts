33cfeb3adc25b048359f926ee0b1b17df8de16d7
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-10-05 12:45:27 +0200

    Drag N Drop - Final

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios";
-import { Project, ProjectFormData } from "@/types/index";
+import { dashboardProjectSchema, editProjectSchema, Project, ProjectFormData, projectSchema } from "@/types/index";
 import { isAxiosError } from "axios";
 
 export async function createProject(formData: ProjectFormData) {
@@ -17,7 +17,7 @@ export async function createProject(formData: ProjectFormData) {
 export async function getProjects() {
     try {
         const { data } = await api.get('/projects');
-        const response = dashboardProjectsSchema.safeParse(data);
+        const response = dashboardProjectSchema.safeParse(data);
         if(response.success) {
             return response.data;
         }
@@ -31,7 +31,24 @@ export async function getProjects() {
 export async function getProjectById(id: Project['_id']) {
     try {
         const { data } = await api.get(`/projects/${id}`);
-        return data
+        const response = editProjectSchema.safeParse(data)
+        if (response.success) {
+            return response.data
+        }
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {        
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getFullProject(id: Project['_id']) {
+    try {
+        const { data } = await api.get(`/projects/${id}`);
+        const response = projectSchema.safeParse(data)
+        if (response.success) {
+            return response.data
+        }
     } catch (error) {
         if(isAxiosError(error) && error.response) {        
             throw new Error(error.response.data.error)
--- src/views/profile/ProfileView.tsx
+++ src/views/profile/ProfileView.tsx
@@ -1,9 +1,8 @@
-import ProjectForm from "@/components/projects/ProjectForm";
+import ProfileForm from "@/components/profile/ProfileForm";
 import { useAuth } from "@/hooks/useAuth";
 
 export default function ProfileView() {
   const { data, isLoading } = useAuth();
-
   if (isLoading) return "Cargando...";
-
-  if(data) return <ProjectForm data={data}/>
+  if (data) return <ProfileForm data={data} />;
+}
